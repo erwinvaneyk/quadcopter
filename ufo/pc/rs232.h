306ce2c6fda2abc8c678b8c650ae4d00783052d3
erwinvaneyk <erwinvaneyk@gmail.com>
2015-09-10 09:57:36 +0200

    Moved the application logic to the separate files

---
+++ ufo/pc/rs232.h
@@ -0,0 +1,21 @@
+
+/*------------------------------------------------------------
+ * Serial I/O 
+ * 8 bits, 1 stopbit, no parity, 
+ * 115,200 baud wired and 9,600 baud wireless.
+ *------------------------------------------------------------
+ */
+#include "protocol.h"
+
+#define SERIAL_DEVICE	"/dev/ttyS0"
+#define USB_DEVICE		"/dev/ttyUSB0"
+#define WIFI_DEVICE 	"/dev/ttyUSB0" /* may need to change this */
+
+int fd_RS232;
+
+int rs232_open(int serial_device);
+int rs232_close(void);
+int	rs232_getchar_nb();
+int rs232_getchar();
+int rs232_putchar(char c);
+int rs232_put_pkt(struct PACKET* packet);
