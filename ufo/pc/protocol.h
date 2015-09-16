90ea711a96e36aa0ef0add3265fe6901bf9bd2b0
erwinvaneyk <erwinvaneyk@gmail.com>
2015-09-16 20:16:27 +0200

    FIX EVERYTHING

--- ufo/pc/protocol.h
+++ ufo/pc/protocol.h
@@ -31,39 +31,39 @@
 
 
 //DATA
-#define HOVER 	0x0000 //level zero
-#define LEVEL1 	0x0001
-#define LEVEL2 	0x0002
-#define LEVEL3 	0x0003
-#define LEVEL4 	0x0004
-#define LEVEL5 	0x0005
-#define LEVEL6 	0x0006
-#define LEVEL7 	0x0007
-#define LEVEL8 	0x0008
- #define LEVEL9 	0x0009
- #define LEVEL10 	0x000A
- #define LEVEL11 	0x000B
- #define LEVEL12 	0x000C
- #define LEVEL13 	0x000D
- #define LEVEL14 	0x000E
- #define LEVEL15 	0x000F
+#define HOVER 	0x00 //level zero
+#define LEVEL1 	0x01
+#define LEVEL2 	0x02
+#define LEVEL3 	0x03
+#define LEVEL4 	0x04
+#define LEVEL5 	0x05
+#define LEVEL6 	0x06
+#define LEVEL7 	0x07
+#define LEVEL8 	0x08
+ #define LEVEL9 	0x09
+ #define LEVEL10 	0x0A
+ #define LEVEL11 	0x0B
+ #define LEVEL12 	0x0C
+ #define LEVEL13 	0x0D
+ #define LEVEL14 	0x0E
+ #define LEVEL15 	0x0F
 
 //1 bit to indicate negative value
-#define LEVEL_N1 	0x0011
-#define LEVEL_N2 	0x0012
-#define LEVEL_N3 	0x0013
-#define LEVEL_N4 	0x0014
-#define LEVEL_N5 	0x0015
-#define LEVEL_N6 	0x0016
-#define LEVEL_N7 	0x0017
-#define LEVEL_N8 	0x0018
- #define LEVEL_N9 	0x0019
- #define LEVEL_N10 	0x001A
- #define LEVEL_N11 	0x001B
- #define LEVEL_N12 	0x001C
- #define LEVEL_N13 	0x001D
- #define LEVEL_N14 	0x001E
- #define LEVEL_N15 	0x001F
+#define LEVEL_N1 	0x11
+#define LEVEL_N2 	0x12
+#define LEVEL_N3 	0x13
+#define LEVEL_N4 	0x14
+#define LEVEL_N5 	0x15
+#define LEVEL_N6 	0x16
+#define LEVEL_N7 	0x17
+#define LEVEL_N8 	0x18
+ #define LEVEL_N9 	0x19
+ #define LEVEL_N10 	0x1A
+ #define LEVEL_N11 	0x1B
+ #define LEVEL_N12 	0x1C
+ #define LEVEL_N13 	0x1D
+ #define LEVEL_N14 	0x1E
+ #define LEVEL_N15 	0x1F
 
 #define NUM_LEVELS 	31
 
@@ -81,6 +81,6 @@ struct PACKET {
 void generate_pkt(struct PACKET* packet, uint8_t mode, uint8_t command, uint16_t data);
 void input_to_pkt(struct INPUT* inputModel, struct PACKET* packet);
 void show_pkt(struct PACKET* packet);
-uint16_t level_convert(int level);
+uint8_t level_convert(int level);
 
 #endif
