035cd8a6f8e815478ab5b25a29b987ca29c2361f
Haji <hajiakhundov@gmail.com>
2015-10-05 21:13:20 +0200

    moved all defines to one file

--- ufo/fpga/defines.h
+++ ufo/fpga/defines.h
@@ -1,11 +1,52 @@
-/*--------------------------
- define protocol specifics
+#define LOGGING
+//#define DEBUG
+#define TRUE 1
+#define FALSE 0
+#define LOG_LENGTH 10000	//=10 seconds of logging in Manual Mode at 1000Hz
+#define THRESHOLD 2000		//(ms) Communicaton safety mechanism threshold value
+
+/*
+ * define some peripheral short hands
  */
 
-#define PACKET_SIZE 6
-//HEADER
-#define HEADER 0xAA //1010 1010
+#define X32_instruction_counter           peripherals[0x03]
+#define X32_timer_per		peripherals[PERIPHERAL_TIMER1_PERIOD]
+//#define X32_timer_per2		peripherals[PERIPHERAL_TIMER2_PERIOD]
+#define X32_leds		peripherals[PERIPHERAL_LEDS]
+#define X32_ms_clock		peripherals[PERIPHERAL_MS_CLOCK]
+#define X32_us_clock		peripherals[PERIPHERAL_US_CLOCK]
+#define X32_QR_a0 		peripherals[PERIPHERAL_XUFO_A0]
+#define X32_QR_a1 		peripherals[PERIPHERAL_XUFO_A1]
+#define X32_QR_a2 		peripherals[PERIPHERAL_XUFO_A2]
+#define X32_QR_a3 		peripherals[PERIPHERAL_XUFO_A3]
+#define X32_QR_s0 		peripherals[PERIPHERAL_XUFO_S0]
+#define X32_QR_s1 		peripherals[PERIPHERAL_XUFO_S1]
+#define X32_QR_s2 		peripherals[PERIPHERAL_XUFO_S2]
+#define X32_QR_s3 		peripherals[PERIPHERAL_XUFO_S3]
+#define X32_QR_s4 		peripherals[PERIPHERAL_XUFO_S4]
+#define X32_QR_s5 		peripherals[PERIPHERAL_XUFO_S5]
+#define X32_display		peripherals[PERIPHERAL_DISPLAY]
+#define X32_QR_timestamp 	peripherals[PERIPHERAL_XUFO_TIMESTAMP]
 
+#define X32_rs232_data		peripherals[PERIPHERAL_PRIMARY_DATA]
+#define X32_rs232_stat		peripherals[PERIPHERAL_PRIMARY_STATUS]
+#define X32_rs232_char		(X32_rs232_stat & 0x02)
+
+#define X32_wireless_data	peripherals[PERIPHERAL_WIRELESS_DATA]
+#define X32_wireless_stat	peripherals[PERIPHERAL_WIRELESS_STATUS]
+#define X32_wireless_char	(X32_wireless_stat & 0x02)
+
+#define X32_button		peripherals[PERIPHERAL_BUTTONS]
+#define X32_switches		peripherals[PERIPHERAL_SWITCHES]
+
+
+/*
+ * define protocol specifics
+ */
+
+#define PACKET_SIZE 7
+//HEADER
+#define HEADER 0xAA
 //MODES or COMMANDS
 #define COMMAND_MODE	0x00
 #define SAFE_MODE		0x01
@@ -28,13 +69,13 @@
 #define LEVEL6 	0x0006
 #define LEVEL7 	0x0007
 #define LEVEL8 	0x0008
- #define LEVEL9 	0x0009
- #define LEVEL10 	0x000A
- #define LEVEL11 	0x000B
- #define LEVEL12 	0x000C
- #define LEVEL13 	0x000D
- #define LEVEL14 	0x000E
- #define LEVEL15 	0x000F
+#define LEVEL9 	0x0009
+#define LEVEL10 	0x000A
+#define LEVEL11 	0x000B
+#define LEVEL12 	0x000C
+#define LEVEL13 	0x000D
+#define LEVEL14 	0x000E
+#define LEVEL15 	0x000F
 
 //1 bit to indicate negative value
 #define LEVEL_N1 	0x0011
@@ -45,10 +86,30 @@
 #define LEVEL_N6 	0x0016
 #define LEVEL_N7 	0x0017
 #define LEVEL_N8 	0x0018
- #define LEVEL_N9 	0x0019
- #define LEVEL_N10 	0x001A
- #define LEVEL_N11 	0x001B
- #define LEVEL_N12 	0x001C
- #define LEVEL_N13 	0x001D
- #define LEVEL_N14 	0x001E
- #define LEVEL_N15 	0x001F
+#define LEVEL_N9 	0x0019
+#define LEVEL_N10 	0x001A
+#define LEVEL_N11 	0x001B
+#define LEVEL_N12 	0x001C
+#define LEVEL_N13 	0x001D
+#define LEVEL_N14 	0x001E
+#define LEVEL_N15 	0x001F
+
+
+ /*
+ * more shorthands
+ */
+
+#define MINIMUM_ENGINE_SPEED 65
+
+#define SAFE_MODE_INT		0
+#define PANIC_MODE_INT		1
+#define MANUAL_MODE_INT		2
+#define CALIBRATE_MODE_INT	3
+#define YAW_CONTROL_INT		4
+#define FULL_CONTROL_INT	5
+
+#define FIFOSIZE 16
+
+#define PANIC_AND_EXIT 	modecommand = PANIC_MODE;\
+						process_packet();\
+						ALIVE = 0;
--- ufo/fpga/main.c
+++ ufo/fpga/main.c
@@ -1,74 +1,17 @@
 /*------------------------------------------------------------------
  *  QR
  *
- *
  *------------------------------------------------------------------
  */
-
-#define LOGGING
-//#define DEBUG
-#define TRUE 1
-#define FALSE 0
-#define LOG_LENGTH 10000	//=10 seconds of logging in Manual Mode at 1000Hz
-#define THRESHOLD 2000		//(ms) Communicaton safety mechanism threshold value
-
 
 #include <stdio.h>
 #include <x32.h>
 #include "assert.h"
-#include "defines.h"
 #include <stdint.h>
-#include "log.h"
 #include <stdlib.h>
+#include "log.h"
+#include "defines.h"
 
-/* define some peripheral short hands
- */
-#define X32_instruction_counter           peripherals[0x03]
-
-#define X32_timer_per		peripherals[PERIPHERAL_TIMER1_PERIOD]
-//#define X32_timer_per2		peripherals[PERIPHERAL_TIMER2_PERIOD]
-
-#define X32_leds		peripherals[PERIPHERAL_LEDS]
-#define X32_ms_clock		peripherals[PERIPHERAL_MS_CLOCK]
-#define X32_us_clock		peripherals[PERIPHERAL_US_CLOCK]
-#define X32_QR_a0 		peripherals[PERIPHERAL_XUFO_A0]
-#define X32_QR_a1 		peripherals[PERIPHERAL_XUFO_A1]
-#define X32_QR_a2 		peripherals[PERIPHERAL_XUFO_A2]
-#define X32_QR_a3 		peripherals[PERIPHERAL_XUFO_A3]
-#define X32_QR_s0 		peripherals[PERIPHERAL_XUFO_S0]
-#define X32_QR_s1 		peripherals[PERIPHERAL_XUFO_S1]
-#define X32_QR_s2 		peripherals[PERIPHERAL_XUFO_S2]
-#define X32_QR_s3 		peripherals[PERIPHERAL_XUFO_S3]
-#define X32_QR_s4 		peripherals[PERIPHERAL_XUFO_S4]
-#define X32_QR_s5 		peripherals[PERIPHERAL_XUFO_S5]
-#define X32_display		peripherals[PERIPHERAL_DISPLAY]
-#define X32_QR_timestamp 	peripherals[PERIPHERAL_XUFO_TIMESTAMP]
-
-#define X32_rs232_data		peripherals[PERIPHERAL_PRIMARY_DATA]
-#define X32_rs232_stat		peripherals[PERIPHERAL_PRIMARY_STATUS]
-#define X32_rs232_char		(X32_rs232_stat & 0x02)
-
-#define X32_wireless_data	peripherals[PERIPHERAL_WIRELESS_DATA]
-#define X32_wireless_stat	peripherals[PERIPHERAL_WIRELESS_STATUS]
-#define X32_wireless_char	(X32_wireless_stat & 0x02)
-
-#define X32_button		peripherals[PERIPHERAL_BUTTONS]
-#define X32_switches		peripherals[PERIPHERAL_SWITCHES]
-
-#define MINIMUM_ENGINE_SPEED 65
-
-#define SAFE_MODE_INT		0
-#define PANIC_MODE_INT		1
-#define MANUAL_MODE_INT		2
-#define CALIBRATE_MODE_INT	3
-#define YAW_CONTROL_INT		4
-#define FULL_CONTROL_INT	5
-
-#define PANIC_AND_EXIT 	modecommand = PANIC_MODE;\
-						process_packet();\
-						ALIVE = 0;
-
-#define FIFOSIZE 16
 uint8_t	fifo[FIFOSIZE]; 
 int	iptr, optr;
 int TERM_CONNECTED = 0; //communication safety mechanism
